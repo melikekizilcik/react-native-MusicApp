078b2c4c21da7f2c2c6dd1d29a1bc0d28a8d9878
Melike <79101522+melikekizilcik@users.noreply.github.com>
2023-03-01 11:27:26 +0300

    Add files via upload

---
+++ SearchBar.styles.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: '#eceff1',
+        padding: 3,
+        margin: 5,
+        borderRadius: 10,
+
+    },
+});
